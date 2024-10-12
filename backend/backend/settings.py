528226377064e06c67d7dc4fd61330725dcfa1b4
adelivuk <adelivuk@gmail.com>
2024-10-12 20:19:25 +0200

    changed the DB name to the services

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 
         'ENGINE': 'mssql',
         'NAME': 'iam',
-        'HOST': '127.0.0.1',
+        'HOST': 'mssql',
         'PORT': '1433',
         'USER': 'sa',
         'PASSWORD': 'admin123!',
@@ -140,7 +140,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000', # BE
+     'http://localhost:3000', # FE
      'http://localhost:5173' # FE
 ]
 
