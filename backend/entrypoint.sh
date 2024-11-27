53c5401abf33ebe45d704c60ae421ef045e49156
adelivuk <adelivuk@gmail.com>
2024-11-27 18:08:10 +0100

    removed superadmin info from docker-compose

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -22,6 +22,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
 
-
-
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! end entrypoint.sh !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
