6824837320a2d1d2bb7d0c3bf1909b89457179fd
adelivuk <adelivuk@gmail.com>
2025-02-05 09:35:07 +0100

    added tostify

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:3000', # FE
      'http://localhost:5173' # FE
+     'http://127.0.0.1:3000' # FE
 ]
 
 # Email configuration
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -8,12 +8,12 @@ function Home() {
         <HomeLayout>
             <div className="flex space-x-4 align-start w-full p-8">
 
-              <div className="max-w-sm p-6 bg-white border border-gray rounded-lg shadow text-center">
-                <Link to="/register" className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Register</Link>
+              <div className="max-w-sm bg-white border border-gray rounded-lg shadow text-center">
+                <Link to="/register" className="mb-2 block p-6 text-2xl font-bold tracking-tight text-gray-900">Register</Link>
               </div>
 
-              <div className="max-w-sm p-6 bg-white border border-gray rounded-lg shadow text-center">
-                <Link to="/forgot-password" className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Forgot Password</Link>
+              <div className="max-w-sm bg-white border border-gray rounded-lg shadow text-center">
+                <Link to="/forgot-password" className="mb-2 block p-6 text-2xl font-bold tracking-tight text-gray-900">Forgot Password</Link>
               </div>
 
             </div>
